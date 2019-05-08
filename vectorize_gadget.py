16c75a7133271346cab16655aacc7d002d38c200
johnb110 <37159799+johnb110@users.noreply.github.com>
2019-05-07 19:40:57 -0500

    Comments and such

--- vectorize_gadget.py
+++ vectorize_gadget.py
@@ -1,18 +1,8 @@
-import sys
-
 import warnings
 warnings.filterwarnings("ignore")
 
-import pandas
 from gensim.models import Word2Vec
 import numpy
-
-
-# DEBUG
-#import joblib
-#import pprint
-#df = joblib.load('df.dump')
-# ##
 
 # Sets for operators
 operators3 = {'<<=', '>>='}
@@ -30,6 +20,12 @@ operators1 = {
     '{', '}'
     }
 
+"""
+Functionality to train Word2Vec model and vectorize gadgets
+Buffers list of tokenized gadgets in memory
+Trains Word2Vec model using list of tokenized gadgets
+Uses trained model embeddings to create 2D gadget vectors
+"""
 class GadgetVectorizer:
 
     def __init__(self, vector_length):
@@ -76,6 +72,10 @@ class GadgetVectorizer:
         res = list(filter(lambda c: c != '', tmp))
         return list(filter(lambda c: c != ' ', res))
 
+    """
+    Tokenize entire gadget
+    Tokenize each line and concatenate to one long list
+    """
     @staticmethod
     def tokenize_gadget(gadget):
         tokenized = []
@@ -83,18 +83,20 @@ class GadgetVectorizer:
             tokenized += GadgetVectorizer.tokenize(line)
         return tokenized
 
+    """
+    Add input gadget to model
+    Tokenize gadget and buffer it to list
+    """
     def add_gadget(self, gadget):
         tokenized_gadget = GadgetVectorizer.tokenize_gadget(gadget)
         self.gadgets.append(tokenized_gadget)
 
-    def vectorize(self, gadget):
-        tokenized_gadget = GadgetVectorizer.tokenize_gadget(gadget)
-        vectors = numpy.zeros(shape=(50, self.vector_length))
-        for i in range(min(len(tokenized_gadget), 50)):
-            vectors[i] = self.embeddings[tokenized_gadget[i]]
-        return vectors
-
+    """
+    Done adding gadgets, now train Word2Vec model
+    Only keep list of embeddings, delete model and list of gadgets
+    """
     def train_model(self):
+        # Set min_count to 1 to prevent out-of-vocabulary errors
         model = Word2Vec(self.gadgets, min_count=1, size=self.vector_length, sg=1)
         self.embeddings = model.wv
         del model
@@ -102,26 +104,12 @@ class GadgetVectorizer:
 
     """
     Uses Word2Vec to create a vector for each gadget
-    Treats lines in gadget as sentences
-    Creates model based on tokens in lines
-    Gets a vector for the gadget by averaging all token vectors
+    Gets a vector for the gadget by combining token embeddings
+    Number of tokens used is min of number_of_tokens and 50
     """
-    def _vectorize_old(self, gadget, vector_size=100):
-        tokenized = []
-        for line in gadget:
-            tokenized.append(GadgetVectorizer.tokenize(line))
-        model = Word2Vec(tokenized, min_count=1, size=vector_size, sg=1)
-        embeddings = model.wv
-        del model
-        vectors = []
-        size = 0
-        for line in tokenized:
-            for token in line:
-                size += 1
-                embedding = embeddings[token]
-                if vectors == []:
-                    vectors = embedding
-                else:
-                    vectors = numpy.add(vectors, embedding)
-        return numpy.true_divide(vectors, size)
-    
+    def vectorize(self, gadget):
+        tokenized_gadget = GadgetVectorizer.tokenize_gadget(gadget)
+        vectors = numpy.zeros(shape=(50, self.vector_length))
+        for i in range(min(len(tokenized_gadget), 50)):
+            vectors[i] = self.embeddings[tokenized_gadget[i]]
+        return vectors
